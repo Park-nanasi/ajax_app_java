9f995bd2f6f699268002aede7f004ce669326484
Park-nanasi <park.sungjin@smart-group.co.jp>
2025-05-01 13:13:50 +0900

    AjaxAppの準備

--- src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
+++ src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
@@ -21,12 +21,8 @@ public class PostController {
   public String showList(Model model) {
     var postList = postRepository.findAll();
     model.addAttribute("postList", postList);
+    model.addAttribute("postForm", new PostForm());
     return "posts/index";
-  }
-
-  @GetMapping("/postForm")
-  public String showPostForm(@ModelAttribute("postForm") PostForm form){
-      return "posts/postForm";
   }
 
   @PostMapping("/posts")
@@ -36,5 +32,5 @@ public class PostController {
     postRepository.insert(post);
     return "redirect:/";
   }
-  
+
 }
